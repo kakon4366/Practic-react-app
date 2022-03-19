8126cdd6fedf3d807c3924a0e1f72a79e06305e2
kakon4366 <kakonbarman4366@gmail.com>
2022-03-19 19:51:06 +0600

    mobile battery up button added

--- src/Components/Mobile/Mobile.js
+++ src/Components/Mobile/Mobile.js
@@ -9,11 +9,19 @@ const Mobile = () => {
         }
         setBattery(battery - 10);
     }
+    
+    const batteryUp = () => {
+        if(battery === 100){
+         return;   
+        }
+        setBattery(battery + 10);
+    }
 
     return (
         <div>
             <h1><span>{battery}</span>%</h1>
             <button onClick={batteryDown}>Battery Down</button>
+            <button onClick={batteryUp}>Battery Down</button>
         </div>
     );
 };
