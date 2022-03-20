09ac84bce9ed99b5226f47a4315bdcdd42364e9d
kakon4366 <kakonbarman4366@gmail.com>
2022-03-20 10:37:39 +0600

    Article Blog button style added

--- src/Components/Article/Article.js
+++ src/Components/Article/Article.js
@@ -13,7 +13,7 @@ const Article = (props) => {
             <article className='blog-article' style={{}}>
                 <h2 style={titleStyle}>This is title {props.articleCount} here!</h2>
                 <p style={{color: 'gray'}}>Lorem ipsum dolor sit amet consectetur adipisicing elit. Officia rerum incidunt maxime nesciunt inventore ut, enim deleniti, alias numquam, nobis in! Adipisci consequuntur, aliquid earum explicabo dolorum impedit tempore, quas molestiae iure, officiis quibusdam porro. Nihil quam pariatur aut eius debitis eum facere dolore ratione, voluptatum fugit dolores repellat. Velit!</p>
-                <button>Show Details</button>
+                <button className='btn btn-secondary'>Show Details</button>
             </article>
         </div>
     );
